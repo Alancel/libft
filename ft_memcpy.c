072f94af72c75cb4e35c88abe1998b7bc7829f96
Михаил Газизулин <alancel@at-o3.msk.21-school.ru>
2020-10-28 19:50:47 +0300

    day 01 ver0.2

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alancel <alancel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 19:36:11 by alancel           #+#    #+#             */
+/*   Updated: 2020/10/28 19:37:50 by alancel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+     size_t i;
+
+	i = 0;
+    if (!dest && !src)
+        return(dest);
+   
+        while (n)
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i++;
+            n--;
+	return (dest);
+}
